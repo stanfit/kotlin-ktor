7b58915bc7533b060cdd6ed16dc1d3073a875d07
MatsushitaKohei <ad.special008@gmail.com>
2020-01-09 21:23:09 +0900

    create debug db cofig

---
+++ database/src/main/kotlin/Database.kt
@@ -0,0 +1,18 @@
+import dao.Users
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.StdOutSqlLogger
+import org.jetbrains.exposed.sql.addLogger
+import org.jetbrains.exposed.sql.transactions.transaction
+
+object Database {
+
+    fun connect() {
+        Database.connect("jdbc:h2:mem:test", driver = "org.h2.Driver")
+    }
+
+    fun create() = transaction {
+        addLogger(StdOutSqlLogger)
+        SchemaUtils.create(Users)
+    }
+}
