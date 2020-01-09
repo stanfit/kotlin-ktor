1fcf563901ff158e229bf0a4196aab4f9b59984d
MatsushitaKohei <ad.special008@gmail.com>
2020-01-09 21:24:04 +0900

    set db in application

--- infrastructure/src/main/kotlin/Application.kt
+++ infrastructure/src/main/kotlin/Application.kt
@@ -3,12 +3,13 @@ import io.ktor.application.install
 import io.ktor.features.CallLogging
 import io.ktor.features.ContentNegotiation
 import io.ktor.http.ContentType
-import io.ktor.routing.routing
+import io.ktor.routing.*
 import io.ktor.serialization.DefaultJsonConfiguration
 import io.ktor.serialization.serialization
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import kotlinx.serialization.json.Json
+import repository.UserRepository
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -16,6 +17,8 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
     embeddedServer(Netty, 8080) {
+        Database.connect()
+        Database.create()
         install(CallLogging)
         install(ContentNegotiation) {
             serialization(
@@ -28,7 +31,9 @@ fun Application.module(testing: Boolean = false) {
             )
         }
         routing {
-            UserController()
+            val repository: UserRepositoryAdapter = UserRepository()
+            val useCase = UserUseCase(repository)
+            UserController(useCase)
         }
     }.start(wait = true)
 }
