04b88c4e59d6dacb5239e62b5dfea84c1a5dd238
MatsushitaKohei <ad.special008@gmail.com>
2020-01-10 21:34:09 +0900

    fix user repository

--- domain/src/main/kotlin/UserRepositoryAdapter.kt
+++ domain/src/main/kotlin/UserRepositoryAdapter.kt
@@ -1,12 +1,12 @@
 interface UserRepositoryAdapter {
 
-    fun create(name: String)
+    fun create(name: String): User?
 
     fun read(): List<User>
 
     fun read(id: Int): User?
 
-    fun update(id: Int, name: String)
+    fun update(id: Int, name: String): User?
 
     fun delete(id: Int)
 }
