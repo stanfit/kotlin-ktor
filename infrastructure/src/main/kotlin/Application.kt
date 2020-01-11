d057edd13654d2441f8a2fc0b64134a302cd635f
MatsushitaKohei <ad.special008@gmail.com>
2020-01-11 16:33:25 +0900

    fix initialize server logic

--- infrastructure/src/main/kotlin/Application.kt
+++ infrastructure/src/main/kotlin/Application.kt
@@ -1,3 +1,4 @@
+import adapter.UserRepositoryAdapter
 import io.ktor.application.Application
 import io.ktor.application.install
 import io.ktor.features.CallLogging
@@ -8,38 +9,38 @@ import io.ktor.locations.Locations
 import io.ktor.routing.Routing
 import io.ktor.serialization.DefaultJsonConfiguration
 import io.ktor.serialization.serialization
+import io.ktor.server.engine.commandLineEnvironment
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import kotlinx.serialization.json.Json
 import repository.UserRepository
-import adapter.UserRepositoryAdapter
 import usecase.UserUseCase
 
-fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+fun main(args: Array<String>) {
+    embeddedServer(Netty, commandLineEnvironment(args)).start(wait = true)
+}
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 @UseExperimental(KtorExperimentalLocationsAPI::class)
 fun Application.module(testing: Boolean = false) {
-    embeddedServer(Netty, 8080) {
-        Database.connect()
-        Database.create()
-        install(Locations)
-        install(CallLogging)
-        install(ContentNegotiation) {
-            serialization(
-                contentType = ContentType.Application.Json,
-                json = Json(
-                    DefaultJsonConfiguration.copy(
-                        prettyPrint = true
-                    )
+    Database.connect()
+    Database.create()
+    install(Locations)
+    install(CallLogging)
+    install(ContentNegotiation) {
+        serialization(
+            contentType = ContentType.Application.Json,
+            json = Json(
+                DefaultJsonConfiguration.copy(
+                    prettyPrint = true
                 )
             )
-        }
-        install(Routing) {
-            val repository: UserRepositoryAdapter = UserRepository()
-            val useCase = UserUseCase(repository)
-            userController(useCase)
-        }
-    }.start(wait = true)
+        )
+    }
+    install(Routing) {
+        val repository: UserRepositoryAdapter = UserRepository()
+        val useCase = UserUseCase(repository)
+        userController(useCase)
+    }
 }
